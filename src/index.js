68fd25f29fba8419c4dbd823f6e366bd8b952056
Maia Borghese <maiaborghese@gmail.com>
2020-08-13 14:37:10 -0700

    some code cleanup

--- src/index.js
+++ src/index.js
@@ -9,57 +9,66 @@ import calculateDistance from "./calculateDistance";
 
 var currentPoints = getPoints40();
 var shortestRouteDistance = null;
-var counter = 0;
+var routeCounter = 0;
 
-const createNewRoute = function(arr, callTracker) {
-
-  if(callTracker > 0) {
-    counter += 1;
-  }
+const createNewRoute = function(arr, recursive) {
 
   const randomRoute = shuffleArray(arr);
   const formattedData = formatData(randomRoute);
-
   const totalDistance = calculateDistance(formattedData);
   const routeCounterElem = document.getElementById("routeCounterMessage").getElementsByTagName('span')[0];
-  routeCounterElem.innerHTML = `${counter}`;
+  const distanceElem =  document.getElementById("distanceMessage");
+  const statusMessage = document.getElementById("statusMessage");
+
+  routeCounter += 1;
+  routeCounterElem.innerHTML = `${routeCounter}`;
 
   if(!shortestRouteDistance || totalDistance < shortestRouteDistance) {
+    
     shortestRouteDistance = totalDistance;
-    const routeMapElem = document.getElementById("routeMap").getElementsByTagName('svg')[0];
-    const distanceElem =  document.getElementById("distanceMessage");
+    
     distanceElem.innerHTML = "";
 
     createGraph(formattedData);
 
     const h3Elem = document.createElement("h3");
     h3Elem.innerHTML = `Total Distance: ${totalDistance} miles`;
-    document.getElementById("distanceMessage").appendChild(h3Elem);
+    distanceMessage.appendChild(h3Elem);
+
   } else {
-     if(counter < 100000) {
-        setTimeout(() => {createNewRoute(currentPoints, 1)}, 0);
-        document.getElementById("statusMessage").style.display = "block";
+     if(routeCounter < 100000) {
+        setTimeout(() => {createNewRoute(currentPoints, true)}, 100);
+        statusMessage.style.display = "block";
       }
   }
 }
 
 document.getElementById("get40CitiesBtn").addEventListener("click", function(){
+  var current = document.getElementsByClassName("active");
+  current[0].className = current[0].className.replace(" active", "");
+  this.className += " active";
   currentPoints = getPoints40();
-  counter = 0;
+  routeCounter = 1;
   shortestRouteDistance = null;
   createNewRoute(currentPoints);
 })
 
 document.getElementById("get200CitiesBtn").addEventListener("click", function(){
+  const activeBtns = document.querySelectorAll(".active");
+  activeBtns.forEach(button => button.classList.remove('active'));
+  this.className += " active";
   currentPoints = getPoints200();
-  counter = 0;
+  routeCounter = 0;
   shortestRouteDistance = null;
   createNewRoute(currentPoints);
 })
 
 document.getElementById("get500CitiesBtn").addEventListener("click", function(){
+  var current = document.getElementsByClassName("active");
+  current[0].className = current[0].className.replace(" active", "");
+  this.className += " active";
   currentPoints = getPoints500();
-  counter = 0;
+  routeCounter = 0;
   shortestRouteDistance = null;
   createNewRoute(currentPoints);
 })
